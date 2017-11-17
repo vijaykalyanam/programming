22445804673bfa7fa065932b6ea28acb3e0234c2
vkalyanam <vkalyanam@lantronix.com>
2017-11-17 15:48:39 +0530

    Recursive function to return string len.

--- c/string/rec_strlen.c
+++ c/string/rec_strlen.c
@@ -2,28 +2,35 @@
 
 int count(char *p)
 {
-	unsigned int c = 0, t;
+	unsigned int t;
 
 	if (*p == '\0')
 		return 0;
-	//To make case Insensitive:
-	//toupper(__ch) (__ch >= 90 && __ch <= 122) ? (__ch = __ch - ('a'-'A')) : (__ch) )
-	if (*p == 'a' || *p == 'e' || *p == 'i'
-			|| *p == 'o' || *p == 'u')
-		c = 1;
-
 	t = count(p+1);
 
-	return c+t;
+	return t+1;
 }
 
+void count2(char *p, int *c)
+{
+	if (*p == '\0') {
+		*c = 0;
+		return;
+	}
+	count2(p+1, c);
+	//*c++; Value is not incrementing.
+	++*c;
+}
 
 int main(void)
 {
 
 	char *p = "aeiou";
+	unsigned int n;
 
-	printf("No of %d\n", noofvowels(p));
+	count2(p, &n);
+
+	printf("No of count: %d count2 :%d\n", count(p), n);
 
 	return 0;
 }
